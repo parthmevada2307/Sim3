1c3e020bb3e703bbd80735eab5ca4be24263c042
BeyondBox Fix <beyondbox@fix.com>
2026-05-04 15:49:44 +0530

    Fix iframe scroll retention cross-navigation

--- sketch.js
+++ sketch.js
@@ -172,6 +172,7 @@ function buildLayout() {
 }
 
 function setup() {
+  window.scrollTo(0, 0);
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
   textFont('Times New Roman');
@@ -549,19 +550,16 @@ function touchStarted() {
   if (touches.length > 0) {
     handlePress(touches[0].x, touches[0].y);
   }
-  return false;
 }
 
 function touchMoved() {
   if (touches.length > 0) {
     handleDrag(touches[0].x, touches[0].y);
   }
-  return false;
 }
 
 function touchEnded() {
   handleRelease();
-  return false;
 }
 
 function handlePress(px, py) {
